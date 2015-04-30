b8512a42f3142ed883fdd84b3010c94d4bc637c5
Chih-Han Liu <andyliu@gate.sinica.edu.tw>
2015-04-30 11:17:45 +0800

    add lab12-1

---
+++ day4/lab12-1/lab12-1.js
@@ -0,0 +1,50 @@
+angular.module('myApp', [
+  "ngRoute"
+])
+.config(["$routeProvider", function($routeProvider) {
+   $routeProvider
+      .when("/", {
+         templateUrl: "ubike.html",
+         controller: 'helloWorld'
+      })
+      .when("/water", {
+         templateUrl: "water.html",
+         controller: 'helloWorld'
+      })
+      .otherwise({ redirectTo: '/' }); 
+}])
+.controller('helloWorld', ['$scope', '$http', '$log', function($scope, $http, $log) {
+  
+   $scope.labName = "ngRoute ubike/water 範例";
+  
+   // 水庫進水量
+   $scope.loadWaterData = function() {
+      $http({url:"http://140.109.6.150:10080/", "method":"GET"})
+        .success(function(data) {
+           $scope.waterData = data.data;
+           angular.forEach($scope.lists, function (list) {
+              list.daliyInflow = parseFloat(list.daliyInflow);
+              list.immediateLevel = parseFloat(list.immediateLevel);
+           });
+        })
+        .error(function(data){  });
+   };
+  
+   // UBike 台北市每五分鐘更新一次資料
+   $scope.reloadData = function() {
+      $scope.areas = [];
+      $http({url:"http://140.109.6.150:10080/ubike", "method":"GET"})
+         .success(function(ubikeData) {
+        console.log(ubikeData);
+            $scope.lists = angular.fromJson(ubikeData);  
+            angular.forEach($scope.lists.retVal, function (list) {
+               if($scope.areas.indexOf(list.sarea) < 0)  
+                  $scope.areas.push(list.sarea);
+            });
+         });
+   };
+  
+   $scope.reloadData();
+   $scope.loadWaterData();
+}])
+;
