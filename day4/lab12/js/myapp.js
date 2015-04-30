3c6075ae250b5f9b002182b24e0dbccf516a59c2
Chih-Han Liu <andyliu@gate.sinica.edu.tw>
2015-04-30 10:27:36 +0800

    add day4 lab12

---
+++ day4/lab12/js/myapp.js
@@ -0,0 +1,21 @@
+angular.module('myApp', [
+  "ngRoute"
+])
+.config(["$routeProvider", function($routeProvider) {
+   $routeProvider
+      .when("/", {
+         template: "<div> hi, This is mainpage</div>",
+         controller: 'helloWorld'
+      })
+      .when("/pageA", {
+         template: "<div> hi, This is page A</div>",
+         controller: 'helloWorld'
+      })
+      .otherwise({ redirectTo: '/' }); 
+}])
+.controller('helloWorld', ['$scope', '$http', '$log', function($scope, $http, $log) {
+  
+   $scope.labName = "ngRoute 範例";
+  
+}])
+;
